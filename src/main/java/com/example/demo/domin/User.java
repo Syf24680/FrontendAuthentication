231dd456e7a5953acff4247410d5b3b25d9211f3
kamintj <s1134961128s@gmail.com>
2024-09-29 19:38:48 +0800

    update entity user

--- src/main/java/com/example/demo/domin/User.java
+++ src/main/java/com/example/demo/domin/User.java
@@ -3,11 +3,15 @@ package com.example.demo.domin;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Date;
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @TableName("sys_user")
 public class User implements Serializable {
     private static final long serialVersionUID=497586889556617583L;
