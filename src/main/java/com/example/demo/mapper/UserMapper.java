ead37501eb6f415299e79a710d86db20862fb565
kamintj <s1134961128s@gmail.com>
2024-09-29 20:16:34 +0800

    userMapper and pom update

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package com.example.demo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.domin.User;
+import org.apache.ibatis.annotations.Mapper;
+
 
 public interface UserMapper extends BaseMapper<User> {
 }
