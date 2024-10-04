111759a4efe744676df553913ec4d574f88b45f6
kamintj <s1134961128s@gmail.com>
2024-10-04 21:14:44 +0800

    UserController and UserService commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,9 @@ package com.example.demo.controller;
 
 import com.example.demo.common.R;
 import com.example.demo.domin.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
     @PostMapping("/login")
     public R login(@RequestBody User user){
-        return null;
+        String jwt=userService.login(user);
+        if (StringUtils.hasLength(jwt)){
+            return R.ok().message("登陆成功").data("token",jwt);
+        }
+        return R.error().message("登录失败");
     }
 }
