1f06602f3cd8b7fb970a534000a8cae5f91a0971
kamintj <s1134961128s@gmail.com>
2024-10-06 04:59:16 +0800

    login authentication commit

--- src/main/java/com/example/demo/filter/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/example/demo/filter/JwtAuthenticationTokenFilter.java
@@ -1,9 +1,15 @@
 package com.example.demo.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.example.demo.domin.vo.LoginUser;
+import com.example.demo.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -23,5 +29,18 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
         if (!StringUtils.hasText(token)){
             throw new RuntimeException("token不存在");
         }
+        String loginUserStr=null;
+        LoginUser loginUser=null;
+        try{
+            Claims claims= JwtUtils.parseJWT(token);
+            loginUserStr=claims.getSubject();
+            loginUser= JSON.parseObject(loginUserStr,LoginUser.class);
+        } catch (Exception e) {
+            throw new RuntimeException("TOKEN 非法");
+        }
+        UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(loginUser,null,null);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request,response);
+
     }
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,15 +1,34 @@
 package com.example.demo.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.demo.domin.User;
+import com.example.demo.domin.vo.LoginUser;
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.service.UserService;
+import com.example.demo.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>implements UserService {
+    @Autowired
+    private AuthenticationManager authenticationManager;
     @Override
     public String login(User user) {
-        return null;
+        UsernamePasswordAuthenticationToken authentication=new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword());
+        Authentication authenticate = authenticationManager.authenticate(authentication);
+        if (Objects.isNull(authenticate)){
+            throw new RuntimeException("登录失败");
+        }
+        LoginUser loginUser =(LoginUser) authenticate.getPrincipal();
+        String loginUserStr = JSON.toJSONString(loginUser);
+        String jwt = JwtUtils.createJWT(loginUserStr, null);
+        return jwt;
     }
 }
